03e67609d2476a6d2d509b2a1e2f2c0ddaf3f5b1
Dima Os <dimaos@proton.me>
2024-02-21 21:29:19 +0300

    Update RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from "@astrojs/rss";
+import { getCollection } from 'astro:content';
 
 export async function get() {
+  const posts = await getCollection("posts");
+
   return rss({
     title: 'Astro!',
     description: 'Juiastro!',
     site: 'https://astro-juiastro.netlify.app/',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>ru-ru</language>`,
   });
 }
